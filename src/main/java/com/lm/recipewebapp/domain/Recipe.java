18708c979f9b7c4864d0a8570496ed9bf29eee86
elleom <el_leo_m@hotmail.com>
2021-09-15 23:21:01 +0200

    added Recipe.java

---
+++ src/main/java/com/lm/recipewebapp/domain/Recipe.java
@@ -0,0 +1,13 @@
+package com.lm.recipewebapp.domain;
+
+public class Recipe {
+    private String description;
+    private String prepTime;
+    private Integer cookTime;
+    private Integer servings;
+    private String source;
+    private String directions;
+    //todo add
+    //private Difficulty difficulty;
+    private Byte[] image;
+}
