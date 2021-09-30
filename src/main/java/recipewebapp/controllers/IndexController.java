85a6a704324a2dc498be0dbcef02ea2d426d5980
elleom <el_leo_m@hotmail.com>
2021-09-30 23:08:17 +0200

    added data pointers

--- src/main/java/recipewebapp/controllers/IndexController.java
+++ src/main/java/recipewebapp/controllers/IndexController.java
@@ -3,8 +3,12 @@ package recipewebapp.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import recipewebapp.domain.Category;
+import recipewebapp.domain.UnitOfMeasure;
 import recipewebapp.repositories.CategoryRepository;
 import recipewebapp.repositories.UnitOfMeasureRepository;
+
+import java.util.Optional;
 
 /***
  *
@@ -23,6 +27,11 @@ public class IndexController {
 
     @RequestMapping({"", "/", "index"}) //see notation , refers to multiple entries
     public String getIndexPage(){
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("teaspoon");
+
+        System.out.println("CAt id is:" + categoryOptional.get().getId());
+        System.out.println("UOM ID is:" + unitOfMeasureOptional.get().getId());
         return "index";
     }
 }
